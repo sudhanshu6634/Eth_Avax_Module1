1d4aa171ecdf3ef7132021deb88edb27f83eeebf
sudhanshu6634 <100783146+sudhanshu6634@users.noreply.github.com>
2023-07-15 15:40:48 +0530

    tempCodeRunner.js

---
+++ tempCodeRunner.js
@@ -0,0 +1,5 @@
+const [ethWallet, setEthWallet] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+  const [ownerName, setOwnerName] = useState(undefined);
